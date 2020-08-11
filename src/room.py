1ff80b8e9812cd4a3b8d0d26f62c0c0f13f449b6
Michelle Scott <scottmm374@gmail.com>
2020-08-11 13:24:59 -0400

    set up classes in player and room

--- src/room.py
+++ src/room.py
@@ -1,2 +1,14 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description, n_to, s_to, e_to, w_to):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def __str__(self):
+        return f"You are currently in {self.name} {self.description}"
